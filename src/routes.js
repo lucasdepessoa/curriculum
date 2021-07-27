3abe273fb285f443c74a2de05e9d668b4bc42c7a
Lucas F Pessoa <lucasdepessoa@gmail.com>
2021-07-27 03:01:45 -0300

    removing previous commit

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,6 @@
 const routes = require('express').Router()
 const resumeController = require('./controllers/resumeController')
 
-//404 route//
-routes.get('*',(req, res)=>{
-  res.send('This route doesn\'t exist yet')
-})
-
 //Main route//
 routes.get('/', (req, res) => {
   res.send('Under development ;D')
