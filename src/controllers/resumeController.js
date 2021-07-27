1758d35ea28a426ca6be45fd7da247c17c644bb3
Lucas F Pessoa <lucasdepessoa@gmail.com>
2021-07-27 02:08:26 -0300

    Experience route created

--- src/controllers/resumeController.js
+++ src/controllers/resumeController.js
@@ -3,6 +3,7 @@ const data = require('../models/resumeAccess')
 class resumeController {
 
   getContact(req, res){
+    
     //Bring me the data//
     let contact = data.getContactInfo()
 
@@ -23,23 +24,52 @@ class resumeController {
   }
 
   getAcademic(req, res){
+
     //Bring me the data//
     let academic = data.getAcademicInfo()
 
-    //build a message//
+    //message start//
     let message = 
     `<dl style="list-style-type:none;">
     <dt>FORMAÇÃO</dt>`
 
-    //build a message, cause here it is a array//
+    //scroll through the array/object to build a message//
     for(let i=0; i<academic.length; i++){
       message += `<dd>${academic[i].school}, ${academic[i].adressSchool} - ${academic[i].status} em ${academic[i].name} - ${academic[i].dateEnd}</dd>`  
     }
 
+    //message end//
     message += '</dl>'
 
 
     //send status 200 and message//
+    res.status(200).send(message)
+  }
+
+  getExperience(req, res){
+
+    //bring me data of job experience//
+    const experience = data.getExperienceInfo()
+
+    //message start//
+    let message = 
+    `<dl style="list-style-type:none">
+      <dt>EXPERIÊNCIA PROFISSIONAL</dt>
+    `
+    //build a message with arrray/object/array//
+    for(let i=0; i<experience.length; i++){
+      message += `<dd> ${experience[i].company.toUpperCase()}, ${experience[i].companyAdress} - ${experience[i].office} DESDE ${experience[i].dateStart} </dd>`
+      
+      //scroll through the array of tasks//
+      for(let j=0; j<experience[i].jobDescription.length; j++){
+        message += `<dd> ${experience[i].jobDescription[j]} </dd>`
+      }
+    }
+    
+    //message end//
+    message += `</dl>`
+  
+    //send status and a message//
     res.status(200).send(message)
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,12 @@ const routes = require('express').Router()
 const resumeController = require('./controllers/resumeController')
 
 //Contact Information//
-routes.get('/contact-info',resumeController.getContact)
+routes.get('/contact-info', resumeController.getContact)
 
 //Academic Information//
-routes.get('/academic-info',resumeController.getAcademic)
+routes.get('/academic-info', resumeController.getAcademic)
+
+//Experience Information//
+routes.get('/experience-info', resumeController.getExperience)
 
 module.exports = routes
