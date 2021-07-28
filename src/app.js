c54ac1039db757457b7b9d6153697324e4743e61
Lucas F Pessoa <lucasdepessoa@gmail.com>
2021-07-28 04:15:28 -0300

    Title changed, assets added, static files configured

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
 const express = require('express')
 
 class AppControll {
-  constructor(){
+  constructor() {
     this.express = express()
 
     this.middlewares()
     this.routes()
   }
 
-  middlewares(){
-    this.express.use(express.json())
-    this.express.set('view engine','ejs')
-    this.express.set('views',__dirname + '/views')
+  middlewares() {
+    //Enable read JSON///
+    this.express.use(express.json())  
+    //Enable ejs view engine//
+    this.express.set('view engine', 'ejs')
+    //Set /views as deafult folder for views//
+    this.express.set('views', __dirname + '/views')
+    //Enable /assets as static path for static files on /assets//
+    this.express.use('/assets', express.static(__dirname + '/assets'))
   }
 
-  routes(){
+  routes() {
     this.express.use(require('./routes'))
   }
 }
