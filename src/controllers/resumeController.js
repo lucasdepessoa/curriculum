66b3df31df1a28c2afd49f187d036b081242b696
Lucas Fernandes Pessoa <lucas192380@br-atacadao.corp>
2021-08-02 21:19:20 -0300

    MainCSS updated; Jquery.slim.min Added; main.js Updated; resumeController Updated; qualifications-info.js Updated; contact-info.js Updated; Index.ejs Updated

--- src/controllers/resumeController.js
+++ src/controllers/resumeController.js
@@ -2,6 +2,7 @@ const data = require('../models/resumeAccess')
 const moment = require('moment')
 const nodeMailer = require('nodemailer')
 const smtp_config = require('../config/smtp')
+const { response } = require('express')
 
 class resumeController {
 
@@ -27,122 +28,40 @@ class resumeController {
           birth: now.diff(data.getContactInfo().birthDate, 'years'),
           hardSkillsLength: Math.ceil(data.getSkillsInfo().hardSkills.length / 2),
           softSkillsLength: Math.ceil(data.getSkillsInfo().softSkills.length / 2),
-          frameworksLength: Math.ceil(data.getSkillsInfo().frameworks.length / 2)
+          frameworksLength: Math.ceil(data.getSkillsInfo().frameworks.length / 2),
+          qualiLentgh: Math.ceil(data.getQualificationsInfo().length / 2)
         }
-
       })
   }
 
   getContact(req, res) {
 
-    //Bring me the data//
-    let contact = data.getContactInfo()
+    res.json(data.getContactInfo())
 
-    //build a message//
-    let message =
-      `
-    <dl style="list-style-type:none;">
-    <dt>INFORMAÇÕES DE CONTATO</dt>
-    <dd>Endereço: ${contact.address}, ${contact.number} - ${contact.district}</dd>
-    <dd>Cidade: ${contact.city}, ${contact.state}, ${contact.cep}</dd>
-    <dd>Celular: ${contact.whatsApp} (WhatsApp)</dd>
-    <dd>E-mail: ${contact.email}</dd>
-    </dl>
-    `
-
-    //send status 200 and message//
-    res.status(200).send(message)
   }
 
   getAcademic(req, res) {
 
-    //Bring me the data//
-    let academic = data.getAcademicInfo()
+    res.status(200).json(data.getAcademicInfo())
 
-    //message start//
-    let message =
-      `<dl style="list-style-type:none;">
-    <dt>FORMAÇÃO</dt>`
-
-    //scroll through the array/object to build a message//
-    for (let i = 0; i < academic.length; i++) {
-      message += `<dd>${academic[i].school}, ${academic[i].adressSchool} - ${academic[i].status} em ${academic[i].name} - ${academic[i].dateEnd}</dd>`
-    }
-
-    //message end//
-    message += '</dl>'
-
-
-    //send status 200 and message//
-    res.status(200).send(message)
   }
 
   getExperience(req, res) {
 
-    //bring me data of job experience//
-    const experience = data.getExperienceInfo()
+    res.status(200).json(data.getExperienceInfo())
 
-    //message start//
-    let message =
-      `<dl style="list-style-type:none">
-      <dt>EXPERIÊNCIA PROFISSIONAL</dt>
-    `
-    //build a message with arrray/object/array//
-    for (let i = 0; i < experience.length; i++) {
-      message += `<dd> ${experience[i].company.toUpperCase()}, ${experience[i].companyAdress} - ${experience[i].office} DESDE ${experience[i].dateStart} </dd>`
-
-      //scroll through the array of tasks//
-      for (let j = 0; j < experience[i].jobDescription.length; j++) {
-        message += `<dd> ${experience[i].jobDescription[j]} </dd>`
-      }
-    }
-
-    //message end//
-    message += `</dl>`
-
-    //send status and a message//
-    res.status(200).send(message)
   }
 
   getSkills(req, res) {
 
-    //bring me data//
-    const skills = data.getSkillsInfo()
+    res.status(200).json(data.getSkillsInfo())
 
-    //message start//
-    let message = `<dl style="list-style-type:none">
-    <dt>HABILIDADES</dt>`
-
-
-    //scroll through the array to build the message"
-    for (let i = 0; i < skills.length; i++) {
-      message += `<dd>${skills[i]}</dd>`
-    }
-
-    //message end//
-    message += '</dl>'
-
-    res.status(200).send(message)
   }
 
   getQualifications(req, res) {
 
-    //bring me data//
-    const qualification = data.getQualificationsInfo()
+    res.status(200).json(data.getQualificationsInfo())
 
-    //message start//
-    let message = `<dl style="list-style-type:none">
-    <dt>QUALIFICAÇÕES TÉCNICAS</dt>`
-
-    qualification.forEach((element, index, arr) => {
-      message += `<dd> ${element} </dd>`
-    });
-
-    //message end//
-    message += '</dl>'
-
-    //send status and a message//
-    res.status(200).send(message)
   }
 
   async sendMail(req, res) {
