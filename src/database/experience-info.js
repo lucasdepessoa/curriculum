bfe4b78859dd25f474b8bd8e9fb7b0884bf7a848
Lucas F Pessoa <lucas.pessoa@finderup.com.br>
2022-04-18 13:34:53 -0300

    atulização de experiência

--- src/database/experience-info.js
+++ src/database/experience-info.js
@@ -4,7 +4,7 @@ module.exports = [
     companyAdress: 'Av. Rio Negro, 1200',
     office: 'Auxiliar de Informática',
     dateStart: '2016-01-06',
-    dateEnd: 'Present',
+    dateEnd: '2021-09-10',
     jobDescription: [
       'Prestar assistência técnica aos usuários internos. (Funcionários)',
       'Análise e manutenção de dados administrativos, financeiros.',
@@ -13,8 +13,51 @@ module.exports = [
       'Manutenção de toda e qualquer estação de trabalho (Computador) na Filial.',
       'Comunicação diária com a Matriz via linux e jobs específicos, através da rotina Batch.',
       'Tratativa do sistema para fechamento do sistema e abertura no dia seguinte.',
-      'Unificação de custos com a Regional aos finais de semana, via linux.',
-      'Desenvolvimento de soluções rápidas, de acordo com a necessidade e com a tecnologia mais adequada.'
+      'Desenvolvimento de soluções rápidas, de acordo com a necessidade e com a tecnologia mais adequada.',
+      'Utilização diária das seguintes tecnologias:',
+      'PHP',
+      'Javascript',
+      'HTML/CSS',
+      'Mysql',
+      'Linux/Shell Script'
+    ] 
+  },
+  {
+    company: 'FINDER UP SOLUCOES EM INFORMATICA LTDA',
+    companyAdress: '-',
+    office: 'Software Engineer Jr.',
+    dateStart: '2021-09-20',
+    dateEnd: '2022-03-20',
+    jobDescription: [
+      'Desenvolvimento de Software Full Stack',
+      'Tecnologias Trabalhadas no Período:',
+      'Angular/React',
+      'NodeJS',
+      'TDD Utilizando Jest + Angular/React',
+      'Metodologia Ágil - KANBAN (Trello e Ferramenta Própria)',
+      'Banco de dados NoSQL Firebase, Postgres',
+      'Contato o tempo todo com TypeScript',
+      'Sistema de Versionamenteo GIT Via GitLab',
+      'Contato com C#, .NET e Visual Basic'
+    ] 
+  },
+  {
+    company: 'BILD DESENVOLVIMENTO IMOBILIÁRIO',
+    companyAdress: '-',
+    office: 'BackEnd Developer',
+    dateStart: '2021-09-20',
+    dateEnd: 'Present',
+    jobDescription: [
+      'Tecnologias trabalhadas durante o período:',
+      'PHP/LARAVEL',
+      'Docker',
+      'Microsoft Azure Storage Explorer para gerenciamento de blobs e implementações via código',
+      'Banco de Dados Oracle, Mysql, SQL-SERVER (SGBDS HeidiSQL, DBeaver)',
+      'VueJS',
+      'Metodoliga Ágil - SCRUM/KANBAN através da ferramenta ClickUp',
+      'Sistema de Versionamento GIT via BitBucket',
+      'Cloud AWS',
+      'SOAP'
     ] 
   }
 ]
