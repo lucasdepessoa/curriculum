d37320d69467c97e46be4751ed94f53eeaa2ac7f
Lucas Pessoa <lucas.pessoa@bild.com.br>
2022-04-27 19:29:37 -0300

    Alteraçãos nas skills + no link whatsapp

--- src/database/skills-info.js
+++ src/database/skills-info.js
@@ -10,11 +10,11 @@ module.exports = {
     { tech: 'PHP', level: 'EXPERT', style: 'bg-secondary', width: '85%', ariaValueNow: '85' },
     { tech: 'WordPress', level: 'ADVANCE', style: 'bg-info', width: '75%', ariaValueNow: '75' },
     { tech: 'JAVA', level: 'MEDIUM', style: 'bg-secondary', width: '50%', ariaValueNow: '50' },
-    { tech: 'TypeScript', level: 'BEGINNER', style: 'bg-secondary', width: '30%', ariaValueNow: '30' },
-    { tech: 'NodeJS', level: 'ADVANCE', style: 'bg-secondary', width: '75%', ariaValueNow: '75' },
+    { tech: 'TypeScript', level: 'ADVANCE', style: 'bg-secondary', width: '75%', ariaValueNow: '75' },
     { tech: 'Git/GitHub', level: 'MASTER', style: 'bg-secondary', width: '95%', ariaValueNow: '95' },
     { tech: 'Lógica de Programação', level: 'INSANE', style: 'bg-secondary', width: '99%', ariaValueNow: '99' },
-    { tech: 'SQL', level: 'MASTER', style: 'bg-secondary', width: '95%', ariaValueNow: '95' }
+    { tech: 'SQL', level: 'MASTER', style: 'bg-secondary', width: '95%', ariaValueNow: '95' },
+    { tech: 'Docker', level: 'MEDIUM', style: 'bg-secondary', width: '50%', ariaValueNow: '50' }
   ],
   softSkills: [
     { tech: 'Trabalho em Equipe', level: 'MASTER', style: 'bg-image', width: '95%', ariaValueNow: '95' },
@@ -28,9 +28,13 @@ module.exports = {
     { tech: 'ReactJS', level: 'MEDIUM', style: 'bg-info', width: '50%', ariaValueNow: '50' },
     { tech: 'Sequelize', level: 'MEDIUM', style: 'bg-secondary', width: '50%', ariaValueNow: '50' },
     { tech: 'JEST (TDD)', level: 'EXPERT', style: 'bg-secondary', width: '85%', ariaValueNow: '85' },
-    { tech: 'Express', level: 'EXPERT', style: 'bg-secondary', width: '85%', ariaValueNow: '85' },
+    { tech: 'Express', level: 'MEDIUM', style: 'bg-secondary', width: '50%', ariaValueNow: '50' },
     { tech: 'Spring', level: 'MEDIUM', style: 'bg-secondary', width: '50%', ariaValueNow: '50' },
     { tech: 'ChartJS', level: 'MEDIUM', style: 'bg-info', width: '50%', ariaValueNow: '50' },
+    { tech: 'Angular', level: 'MEDIUM', style: 'bg-info', width: '50%', ariaValueNow: '50' },
+    { tech: 'NodeJS', level: 'ADVANCE', style: 'bg-secondary', width: '75%', ariaValueNow: '75' },
+    { tech: 'VueJS', level: 'MEDIUM', style: 'bg-secondary', width: '50%', ariaValueNow: '50' },
+    { tech: 'LARAVEL', level: 'ADVANCE', style: 'bg-secondary', width: '75%', ariaValueNow: '75' }
   ],
   observationSkills: [
     'Estou familiarizado com rotinas de desenvolvimento Full-Stack.',
@@ -40,6 +44,8 @@ module.exports = {
     'Conhecimento e Experiência em modelagem de dados.',
     'Conhecimento avançado e experiência em algoritmos e lógica de programação.',
     'Experiência em resolução de problemas de maneira ágil.',
-    'Organização de projetos/rotinas utilizando Kanban/Scrum.'
+    'Organização de projetos/rotinas utilizando Kanban/Scrum.',
+    'Príncipios SOLID.',
+    'Código Limpo.'
   ]
 }
